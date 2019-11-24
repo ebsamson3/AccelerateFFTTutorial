4448c9a1bed76f1d98f99f70af850cb3fa3a63d1
ebsamson3 <ebsamson3@gmail.com>
2019-11-24 14:04:20 -0600

    Initial Commit

---
+++ AccelerateFFT/AccelerateFFT.h
@@ -0,0 +1,19 @@
+//
+//  AccelerateFFT.h
+//  AccelerateFFT
+//
+//  Created by Edward Samson on 11/24/19.
+//  Copyright © 2019 Edward Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccelerateFFT.
+FOUNDATION_EXPORT double AccelerateFFTVersionNumber;
+
+//! Project version string for AccelerateFFT.
+FOUNDATION_EXPORT const unsigned char AccelerateFFTVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccelerateFFT/PublicHeader.h>
+
+
